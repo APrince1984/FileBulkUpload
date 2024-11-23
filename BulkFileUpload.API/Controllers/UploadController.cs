d5548b97e27e0cdc6bf216659f9940bea4f9e025
Anthony de Prince <AnthonydePrince@gmail.com>
2024-11-23 11:40:47 +0100

    Basic setup

---
+++ BulkFileUpload.API/Controllers/UploadController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkFileUpload.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+    }
+}
